cfc46043820da3f55975e933335380db2c53cb98
Andres Santos <dresrok@gmail.com>
2022-02-17 00:24:41 -0500

    Adding solution files

---
+++ solution_test.go
@@ -0,0 +1,16 @@
+package solution
+
+import (
+	"testing"
+
+	"github.com/kyokomi/emoji/v2"
+)
+
+func TestGetMessage(t *testing.T) {
+	hello := GetMessage()
+	expected := emoji.Sprint("Hello, :world_map:")
+
+	if hello != expected {
+		t.Errorf("hello %q expected %q", hello, expected)
+	}
+}
